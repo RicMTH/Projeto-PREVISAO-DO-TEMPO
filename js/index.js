f907f54fec3660620ff3eabbbd362c7cbaa141d3
RMMENGAO <ricmrds@gmail.com>
2025-09-17 16:16:24 -0300

    .

--- js/index.js
+++ js/index.js
@@ -6,21 +6,29 @@ form?.addEventListener('submit', async (event) => {
     if (!input || !sectionTempoInfo)
         return;
     const localizacao = input.value;
-    const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=9d9ac1ee71f15eb488cbff6181a21a28&lang=pt_br&units=metric`);
-    const dados = await resposta.json();
-    const infos = {
-        temperatura: Math.round(dados.main.temp),
-        local: dados.name,
-        icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
-    };
-    sectionTempoInfo.innerHTML = `
+    console.log(input);
+    try {
+        const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=9d9ac1ee71f15eb488cbff6181a21a28&lang=pt_br&units=metric`);
+        const dados = await resposta.json();
+        const infos = {
+            temperatura: Math.round(dados.main.temp),
+            localizacao: dados.name,
+            icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+        };
+        sectionTempoInfo.innerHTML = `
         <div class="tempo-dados">
-            <h2>${infos.local}</h2>
+            <h2>${infos.localizacao}</h2>
             <span>${infos.temperatura}°C</span>
             
         </div>
         <img src="${infos.icone}"/>
     `;
+        input.value = '';
+    }
+    catch (error) {
+        alert('Localização não encontrada');
+        console.error(error);
+    }
 });
 export {};
 //# sourceMappingURL=index.js.map
